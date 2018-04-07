8969d7f7cc06dca8d9149b45eb0419a9b2509bb8
Alex Li <alexlive@umich.edu>
2018-04-07 14:25:10 -0400

    Sample data, fixed crawler.py

--- crawler.py
+++ crawler.py
@@ -2,25 +2,36 @@ from bs4 import BeautifulSoup
 import time	
 import requests
 import sys
+import json
 
 BASE = 'https://www.mayoclinic.org'
 URLS = []
 SEEN = {}
+DB = {}
 
 def scrapePage(url, desc):
 	print('@@@ ScrapePage: {}'.format(url))
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, 'html.parser')
-	h2s= soup.find_all('h2')
+	# h2s= soup.find_all('h2')
+	content = soup.find('div', class_="content")
+	h2s= content.find_all('h2')
 	for h2 in h2s:
 		if h2.text == 'Symptoms':
 			next_el = h2.findNext('p')
-			while next_el.text[-1] != ':':
+			DB[desc] = {}
+			DB[desc]['text'] = ''
+			while next_el != None and next_el.text[-1] != ':' :
+				DB[desc]['text'] += next_el.text
 				print(next_el.text)
-				next_el = next_el.findNext('p')
+				next_el = next_el.find_next_sibling('p')
+			DB[desc]['symptoms_list'] = []
+			if next_el == None:
+				return
 			symptoms_list = next_el.findNext('ul')
 			symptoms = symptoms_list.find_all('li')
 			for symp in symptoms:
+				DB[desc]['symptoms_list'].append(symp.text)
 				print('\t{}'.format(symp.text))
 def scrapeList(url):
 	print('### ScrapeList: {}'.format(url))
@@ -32,9 +43,9 @@ def scrapeList(url):
 	for letter in letters:
 		extension = letter['href']
 		next_url = BASE + extension
-		if next_url not in SEEN:
-			results.append((next_url, letter.text))
+		if '(See:' not in letter.text and next_url not in SEEN:
 			SEEN[next_url] = True
+			results.append((next_url, letter.text))
 			print('Page:' + next_url)
 	return results
 
@@ -42,6 +53,8 @@ def crawlPages(seed_url):
 	# add to seen 
 	QUEUE = []
 	SEEN[seed_url] = True
+	# hard code ignore
+	SEEN['https://www.mayoclinic.org#index'] = True
 	# start with index
 	response = requests.get(seed_url)
 	soup = BeautifulSoup(response.text, 'html.parser')
@@ -51,7 +64,6 @@ def crawlPages(seed_url):
 		extension = letter['href']
 		next_url = BASE + extension
 		if letter.text <= 'Z' and letter.text >= 'A':
-			print(next_url)
 			key = 'letter' + letter.text
 			QUEUE.append((next_url, key))
 	# start crawling]
@@ -61,10 +73,16 @@ def crawlPages(seed_url):
 		QUEUE = QUEUE[1:]
 		# get list page
 		if 'letter' in t:
-			QUEUE += scrapeList(current_url)
+			results = scrapeList(current_url) 
+			QUEUE += results
+			print('$$$ {} : {}'.format(current_url, results))
 		# get specific page
 		else:
 			scrapePage(current_url, t)
 
 if __name__ == '__main__':
+	# scrapePage('https://www.mayoclinic.org/diseases-conditions/baby-acne/symptoms-causes/syc-20369880', 'Baby Acne')
 	crawlPages('https://www.mayoclinic.org/diseases-conditions/index')
+	with open('mayoclinic.txt', 'w') as outfile:
+		json.dump(DB, outfile)
+	

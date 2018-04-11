443866461099600b83265586bc84581672a795b8
Alex Li <alexlive@umich.edu>
2018-04-11 14:29:41 -0400

    added preprocessing

---
+++ preprocess.py
@@ -0,0 +1,56 @@
+# Alexander Li
+# alexlive
+from nltk.stem.porter import *
+import nltk
+import sys
+import string
+import glob
+import os
+
+STOPWORDS = set()
+
+# load stopwords
+def loadStopwords():
+	file_obj = open('stopwords', 'r')
+	lines = file_obj.readlines()
+	for line in lines:
+		STOPWORDS.add(line.strip())
+	file_obj.close()
+
+# tokenize text
+def tokenizeText(text):
+	return nltk.word_tokenize(text)
+
+# convert list to freq dict
+def dictify(tokens, dic = {}):
+	for t in tokens:
+		if t not in dic:
+			dic[t] = 0
+		dic[t] += 1
+	return dic[t]
+
+# remove stopwords
+def removeStopwords(data):
+	return [i for i in data if i not in STOPWORDS]
+
+# stem words
+def stemWords(tokens):
+	inst = PorterStemmer()
+	result = []
+	for t in tokens:
+		result.append(inst.stem(t))
+	return result
+
+def preprocess(text):
+	loadStopwords()
+	data = text.lower().strip()
+	# tokenize
+	tokens = tokenizeText(data)
+	# filter out stopwords
+	filtered = removeStopwords(tokens)
+	# stem tokens
+	stemmed = stemWords(filtered)
+	return stemmed
+
+if __name__ == '__main__':
+	main()

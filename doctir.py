2744f8e327cc0b28e2c12fd3430c705d44d18a28
miyole <mlee1746@gmail.com>
2018-04-18 14:55:00 -0400

    things

--- doctir.py
+++ doctir.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 
 
 def read_illness_data(filenames):
+    # Read in the illness dictionaries from the given files.
     sources = []
     for filename in filenames:
         with open(filename) as file:
@@ -16,36 +17,39 @@ def read_illness_data(filenames):
 
 
 def get_tokens(files):
+    # Get the text and symptoms tokens for the given files
     sources = read_illness_data(files)
-    text_tokens = {}
-    symptoms_tokens = {}
+    text_tokens = defaultdict(list)
+    symptoms_tokens = defaultdict(list)
     for data in sources:
         for illness in data:
-            if len(data[illness]['symptoms_list']):
-                for symptom in data[illness]['symptoms_list']:
-                    symptoms_tokens[illness] = preprocess(symptom)
-                    symptoms_tokens[illness].extend(preprocess(symptom, 2))
-            else:
-                text_tokens[illness] = preprocess(data[illness]['text'])
-                text_tokens[illness].extend(preprocess(data[illness]['text'], 2))
+            add_data(text_tokens[illness],
+                     symptoms_tokens[illness],
+                     data[illness])
 
     return text_tokens, symptoms_tokens
 
 
-def expand_query(tokens):
-    query = ''
-    for token in tokens:
-        ss = wn.synsets(token)
-        if len(ss):
-            for syn in wn.synsets(token):
-                for lemma in syn.lemma_names():
-                    query += ' ' + ' '.join(lemma.split('_'))
-        else:
-            query += ' ' + token
-    return query
+def add_data(text, symptoms, illness_data):
+    # If there's a symptoms list for this illness, then add it to the symptoms list.
+    # Otherwise, add the text to the text list.
+    if len(illness_data['symptoms_list']):
+        for symptom in illness_data['symptoms_list']:
+            symptoms.extend(preprocess(symptom))
+            symptoms.extend(preprocess(symptom, 2))
+    else:
+        text.extend(preprocess(illness_data['text']))
+        text.extend(preprocess(illness_data['text'], 2))
+    return text, symptoms
 
 
 def prepare_vector_space_model():
+    '''
+    Prepares the Wikipedia vector space model for internal querying (for
+    illness name normalization) and the combined data vector space model.
+    '''
+
+    # If the combined vector space model already exists, then load it and return
     combined_model = 'model.pkl'
     if os.path.isfile(combined_model):
         print('Loading combined vector space model...')
@@ -53,50 +57,48 @@ def prepare_vector_space_model():
             return dill.load(infile)
         print('Done!')
 
+    # If the Wikipedia vector space model exists, then load it.
+    # Otherwise, create it and then save it.
     wiki_model = 'wiki_model.pkl'
+    print('Reading Wikipedia text and symptoms tokens...')
+    wiki_text, wiki_symptoms = get_tokens(['wikipedia.txt'])
+    print('Done!')
     if os.path.isfile(wiki_model):
         print('Loading Wikipedia vector space model...')
         with open(wiki_model, 'rb') as infile:
             vsm = dill.load(infile)
     else:
+        # Don't use the symptoms to build the Wikipedia vector space model.
+        # Only use the text so that illness name normalization works better.
         print('Creating Wikipedia vector space model...')
         vsm = VectorSpaceModel(doc_wt_scheme='tfc', query_wt_scheme='nfx')
-        text_tokens, symptoms_tokens = get_tokens(['wikipedia.txt'])
-        vsm.prepare(text_tokens, 1., True)
-        vsm.prepare(symptoms_tokens, 2.)
+        vsm.prepare(wiki_text, 1.)
         print('Dumping...')
         with open(wiki_model, 'wb') as outfile:
             dill.dump(vsm, outfile)
     print('Done!')
     print('Creating combined vector space model...')
 
-    #Merging illnesses
+    # Merging illnesses
     illness_files = ['cdc.txt', 'mayoclinic.txt']
     sources = read_illness_data(illness_files)
-
     merged_symptoms = defaultdict(list)
     merged_text = defaultdict(list)
     for data in sources:
         for illness in data:
+            # Internally query vector space model to normalize illness names.
             query = preprocess(illness) + preprocess(illness, 2)
             results = vsm.retrieve_ranked_docs(query)
             if len(results):
-                #print('Query: {}; Returned: {}'.format(query, results[0]))
                 normed_illness = results[0][0]
-                print(query, results[0])
             else:
                 normed_illness = illness
-            print('Actual illness: {}'.format(illness))
-            print('Normed illness: {}'.format(normed_illness))
-            if len(data[illness]['symptoms_list']):
-                for symptom in data[illness]['symptoms_list']:
-                    merged_symptoms[normed_illness].extend(preprocess(symptom))
-                    merged_symptoms[normed_illness].extend(preprocess(symptom, 2))
-            else:
-                merged_text[normed_illness].extend(preprocess(data[illness]['text']))
-                merged_text[normed_illness].extend(preprocess(data[illness]['text'], 2))
+            add_data(merged_text[normed_illness],
+                     merged_symptoms[normed_illness],
+                     data[illness])
 
-    vsm.prepare(merged_text, 1., True)
+    vsm.prepare(wiki_symptoms, 2., only_index=True)
+    vsm.prepare(merged_text, 1., only_index=True)
     vsm.prepare(merged_symptoms, 2.)
     print('Dumping...')
     with open(combined_model, 'wb') as outfile:
